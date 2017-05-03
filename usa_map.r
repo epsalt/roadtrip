2e480a90170ae1b27ee14b6dc72ccd7b6b01fbb7
epsalt <evan.saltman@gmail.com>
2017-05-03 15:36:14 -0600

    Map improvements

--- usa_map.r
+++ usa_map.r
@@ -2,6 +2,7 @@ library(RJSONIO)
 library(ggplot2)
 library(magrittr)
 library(maps)
+library(ggrepel)
 
 loc_file <- "data/loc_history.json"
 
@@ -38,13 +39,31 @@ trip <- ldf[ldf$date > as.Date("2016-07-28") &
 trip$long <- trip$longitudeE7 / 10^7
 trip$lat <- trip$latitudeE7 / 10^7
 
-## Map stuff
 ## Remove outliers
 trip <- trip[trip$long < -50,]
 trip <- trip[trip$lat > 33,]
 
+## Map stuff
+
+## Data
 states <- map_data("state")
+cities <- read.csv("data/cities.csv")
+
 trip_map <- ggplot()+
-         geom_map(data=states, map=states, aes(x=long, y=lat, map_id=region),
-                  fill="#ffffff", color="grey70", size=0.15)+
-         geom_path(data=trip, aes(x=long, y=lat))
+         ggtitle("Roadtrip 2016", subtitle="NYC to YVR")+ 
+         geom_map(data=states, map=states,
+                  aes(x=long, y=lat, map_id=region),
+                  fill="#ffffff", color="grey70", size=0.4)+
+         geom_path(data=trip, aes(x=long, y=lat), linetype=1)+
+         geom_point(data=cities, color="red",
+                   aes(x=long, y=lat, group=NULL))+
+         geom_label_repel(data=cities, aes(x=long, y=lat, label=city),
+                          label.size=0, size=3,
+                          fill=rgb(1,1,1,0.6),
+                          label.padding=unit(0, "lines"),
+                          box.padding=unit(0.15, "lines"),
+                          point.padding=unit(0.15, "lines"))+
+         coord_map()+
+         theme_void()+
+         theme(plot.title = element_text(hjust = 0.5),
+               plot.subtitle = element_text(hjust = 0.5))
